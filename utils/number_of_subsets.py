1f973535c89a3f868cc56dafdb61363d40c6c210
alexdenker <adenker@uni-bremen.de>
2024-09-19 13:08:14 +0100

    number of subsets for TOF

--- utils/number_of_subsets.py
+++ utils/number_of_subsets.py
@@ -2,7 +2,7 @@
 import numpy as np 
 import math 
 
-def compute_number_of_subsets(views):
+def compute_number_of_subsets_non_tof(views):
     """
     At least 8 views per subset
     
@@ -30,12 +30,70 @@ def compute_number_of_subsets(views):
         subset_number =  combined[0][0] 
     else:
         num_div, subset_size, num_primes = list(zip(*combined_filtered))
-
         amax = np.argmax(num_primes)
 
         subset_number = num_div[amax]
 
     return subset_number
+
+def compute_number_of_subsets_tof(views):
+    """
+    At least 8 views per subset
+    
+    """
+    
+    """
+    n_subs = math.floor(views / 8)
+    # views has to be divisible by n_subs
+
+    while views % n_subs != 0:
+        n_subs -= 1
+
+        if n_subs < 1: break 
+    return int(max(n_subs, 1))
+    """
+    num_divisors = list(divisorGenerator(views))
+    subset_size = [int(views / div) for div in num_divisors]
+    num_primes = [len(prime_factors(div)) for div in num_divisors]
+
+    combined = list(zip(num_divisors, subset_size, num_primes))
+    #combined_filtered = [(x,y,z) for (x,y,z) in combined if y >= 8]
+
+    combined_filtered = combined[1:]
+    combined_filtered = combined_filtered[:-1]
+
+    if len(combined_filtered) < 2:
+        subset_number =  combined[0][0] 
+    else:
+        combined_filtered = combined_filtered[::-1]
+        num_div, subset_size, num_primes = list(zip(*combined_filtered))
+
+        amax = np.argmax(num_primes)
+        subset_number = num_div[amax]
+    if len(combined_filtered) == 0:
+        return views
+
+    return subset_number
+
+def compute_number_of_subsets(views, tof):
+    """
+    views: number of view
+    tof: bool, tof == True, we have time of flight data
+    
+    """
+
+    try:
+        if tof:
+            num_subsets = compute_number_of_subsets_tof(views)
+        else:
+            num_subsets = compute_number_of_subsets_non_tof(views)
+    except IndexError:
+        if views % 2 == 0:
+            num_subsets = views // 2
+        else:
+            num_subsets = views
+
+    return num_subsets
 
 def prime_factors(n):
     i = 2
@@ -63,10 +121,14 @@ def divisorGenerator(n):
 
 if __name__ == "__main__":
 
-    for num in [50, 252, 128]:
-        print("Test for ", num, " -> ", compute_number_of_subsets(num), " subsets")
+    #for num in [50, 252, 128, 34, 645, 646]:
+    #    print("Test for ", num, " -> ", compute_number_of_subsets(num), " subsets")
 
-            
+    #for num in [50, 252, 128, 34, 645, 646]:
+    #    print("Test TOF for ", num, " -> ", compute_number_of_subsets_tof(num), " subsets")
+
+    for num in range(40, 400):
+        print(num, compute_number_of_subsets(num, tof=True))
 
     """
 
